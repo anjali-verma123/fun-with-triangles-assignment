fd570ced0a65b7a63cdb71e4db441164aa543021
Anjali Verma <av498565@gmail.com>
2022-12-18 20:41:19 +0530

    docs index.html, quiz.html, index2.html, quiz.js
    Button inside div element
    JS code related to quiz.js

--- quiz.js
+++ quiz.js
@@ -1,6 +1,6 @@
 const quizForm = document.querySelector(".quiz-form");
 const submitAnswerBtn = document.querySelector("#submit-answer-btn");
-const outputE1 = document.querySelector('#output');
+const outputE1 = document.querySelector('.output');
 const correctAnswers = ["90", "right angled"];
 
 function calculateScore() {
